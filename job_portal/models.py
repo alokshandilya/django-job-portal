226785136ba5152903fb0ade08230ab8955788fd
alokshandilya <alok.shandilya10@gmail.com>
2024-10-12 08:37:36 +0530

    feat: add Job model

--- job_portal/models.py
+++ job_portal/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Job(models.Model):
+    # id starts at 1 and autoincrements
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    company = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
