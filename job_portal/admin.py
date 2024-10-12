82519537a8e86d0c95a65d1a52e570132303e840
alokshandilya <alok.shandilya10@gmail.com>
2024-10-12 23:15:59 +0530

    feat: add Job model to admin panel and update job display in index page

--- job_portal/admin.py
+++ job_portal/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from job_portal.models import Job
 
 # Register your models here.
+admin.site.register(Job)
--- job_portal/models.py
+++ job_portal/models.py
@@ -9,3 +9,8 @@ class Job(models.Model):
     company = models.CharField(max_length=100)
     salary = models.IntegerField()
     is_active = models.BooleanField(default=False)
+
+    # for django admin panel
+    def __str__(self):
+        active = "active" if self.is_active else "not active"
+        return f"{self.id}: {self.title} at {self.company} | {active}"
